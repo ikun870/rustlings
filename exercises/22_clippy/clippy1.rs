1e88d71283fbafdd866712422598ccba07608758
ikun870 <pro8720@outlook.com>
2023-11-26 22:56:23 +0800

    commit

--- exercises/22_clippy/clippy1.rs
+++ exercises/22_clippy/clippy1.rs
@@ -10,13 +10,13 @@
 // hint.
 
 
-
+use std::f32;
 
 fn main() {
-    let pi = 3.14f32;
+    let pi = f32::consts::PI;
     let radius = 5.00f32;
 
-    let area = f32::consts::PI * f32::powi(radius, 2);
+    let area = pi * f32::powi(radius, 2);
 
     println!(
         "The area of a circle with radius {:.2} is {:.5}!",
--- exercises/23_conversions/from_str.rs
+++ exercises/23_conversions/from_str.rs
@@ -31,8 +31,6 @@ enum ParsePersonError {
     ParseInt(ParseIntError),
 }
 
-// I AM NOT DONE
-
 // Steps:
 // 1. If the length of the provided string is 0, an error should be returned
 // 2. Split the given string on the commas present in it
@@ -52,6 +50,28 @@ enum ParsePersonError {
 impl FromStr for Person {
     type Err = ParsePersonError;
     fn from_str(s: &str) -> Result<Person, Self::Err> {
+        if s.len() == 0 {
+            return Err(ParsePersonError::Empty);
+        }
+        
+        let parts: Vec<&str> = s.split(',').collect();
+        if parts.len() != 2 {
+            return Err(ParsePersonError::BadLen);
+        }
+        
+        let name = parts[0];
+        if name.is_empty() {
+            return Err(ParsePersonError::NoName);
+        }
+        
+        // let age_result = parts[1].parse::<usize>();
+        // match age_result {
+        //     Ok(age) => Ok(Person{name:name.into(),age:age_result}),
+        //     Err(_) => Err(ParsePersonError::arseInt(ParseIntError)),
+        // }
+        let age_result = parts[1].parse::<usize>().map_err(ParsePersonError::ParseInt)?;
+        Ok(Person { name: name.into(), age: age_result })
+
     }
 }
 
